48c8b9d97fa4315ce434974066ddead6d8c22255
dayueqq <2252990046@qq.com>
2014-02-07 15:46:35 +0800

    2014-2-7

--- web/common/sql.inc.php
+++ web/common/sql.inc.php
@@ -1,5 +1,5 @@
 <?php
-  $sql=mysql_connect('localhost','hyc','mTqXpUC7pbNSYDV3');
+  $sql=mysql_connect('localhost','hyc','hycgdufs');
   $db=mysql_select_db("report",$sql);
   mysql_query('set names utf8');
 ?>
